82bf01a393e28010c378d7a729dbccd268f5a2d0
Shazima kouser <shazima028@gmail.com>
2025-05-20 17:37:35 +0530

    commit

--- moviereviewapp/src/components/MovieCard.jsx
+++ moviereviewapp/src/components/MovieCard.jsx
@@ -1,12 +1,3 @@
-// const MovieCard = ({ poster, title, children }) => (
-//     <div className="p-4 bg-white shadow rounded-xl w-72 m-4">
-//       <img src={poster} alt={title} className="w-full h-96 object-cover rounded" />
-//       <h2 className="text-xl font-bold mt-2">{title}</h2>
-//       {children}
-//     </div>
-//   );
-  
-//   export default MovieCard;
 const MovieCard = ({ title, poster, children }) => (
   <div className="movie-card">
     <img src={poster} alt={title} className="movie-poster" />
